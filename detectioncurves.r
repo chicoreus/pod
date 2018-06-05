40d8d10e9eecc087b7309afcddc5802e47039ee3
Paul J. Morris <mole@morris.net>
2018-06-04 21:25:27 -0400

    Second cut at producing detection function graphs, still not at desired result.

--- detectioncurves.r
+++ detectioncurves.r
@@ -79,141 +79,320 @@ lines(coverage,pod,col="blue")
 coverage_exp<-coverage
 pod_exp<-pod
 
-
 dev.off();
 
 HTMLInsertGraph(graphpc,file=output,Caption="Coverage POD relationships for exponetnials and perfect broom detection fuctions")
 
-graphpcs<-"coveragepodsim.png"
-png(file.path(outputDirectory,graphpcs),width=1200,height=1200)
+graphpcs1<-"coveragecurves.png"
+png(file.path(outputDirectory,graphpcs1),width=1200,height=1200)
+
+f<-function(x,s) { exp(-abs(x-s))}
+lp<-function(f,x,spacing) { 
+    r<-c()
+    offsets<-seq(-1,12)
+    for (offset in offsets) { 
+       r<-c(r,f(x,offset*spacing))
+    }
+    return(r)
+}
+
+x<-c(0,10)
+y<-c(0,1)
+plot(x,y,type="p",xlim=c(0,10),ylim=c(0,1),main="Exponential detection function")
+
+# simple case, probability of any one of a list of probabilities.
+pUnionAnyOneOf <- function(p){ 1 - prod(1-p) } 
+
+# General implementations, probability of n of a list of probabilities: 
+# See: https://stackoverflow.com/questions/35119185/probability-of-the-union-of-three-or-more-sets
+cp <- function(p) 
+{
+  # slow
+  ev <- do.call(expand.grid,replicate(length(p),0:1,simplify=FALSE))
+  pe <- apply(ev,1,function(x) prod(p*(x==1)+(1-p)*(x==0)))
+  tapply(pe,rowSums(ev),sum)
+}
+cp.quadratic <- function(p) {
+  P <- matrix(0, nrow=length(p), ncol=length(p))
+  P[1,] <- rev(cumsum(rev(p * prod(1-p) / (1-p))))
+  for (i in seq(2, length(p))) {
+    P[i,] <- c(rev(cumsum(rev(head(p, -1) / (1-head(p, -1)) * tail(P[i-1,], -1)))), 0)
+  }
+  c(prod(1-p), P[,1])
+}
 
 coverages<-c()
 pods<-c()
-widths<-c(1.5,2,2.5,3,3.5,4,5,6,7,8,9,10,11,12,14,16,20,24,28,36,46,66)
+sweepsPerWidth<-c(1,2,3,4,5,6,7,8,9,10)
+width<-10
 f<-function(x) {exp(-abs(x))}
 area<-integrate(f,-100,100)
-halfSweepWidth<-area$value/2  
-for (width in widths) { 
+sweepWidth<-area$value
+f<-function(x,s) { exp(-abs(x-s))}
+for (sp in sweepsPerWidth) { 
+   spacing = width / sp
    hits=0
-   misses=0
-   # put one curve half way through area, one centered on each end.  
-   spacing = width/2  
-   f1<-function(x) {exp(-abs(x-spacing))}
-   f2<-function(x) {exp(-abs(x-(spacing*2)))}
-   # also put one curve at the next interval past the end and one before zero.
-   f3<-function(x) {exp(-abs(x-(spacing*3)))}
-   f4<-function(x) {exp(-abs(x-(-spacing)))}
-   for (i in 1:10000) { 
-      x <- runif(1,0,width)
-      detect <- f(x)
-      detect1 <- f1(x)
-      detect2 <- f2(x)
-      detect3 <- f3(x)
-      detect4 <- f4(x)
-      p<-runif(1,0,1)
-      if (p<detect) { hits<-hits+1 } else { 
-         if (p<detect1) { hits<-hits+1 } else { 
-           if (p<detect2) { hits<-hits+1 } else { 
-           if (p<detect3) { hits<-hits+1 } else { 
-           if (p<detect4) { hits<-hits+1 } else { 
-             misses<-misses+1
-           }
-           }
-           }
-         }
-      }
+   tries=10000
+   for (i in 1:tries) { 
+      x <- runif(1,min=0,max=width)
+      thresholds <- lp(f,x,spacing)
+
+if (sp==1) { 
+xes<-c()
+for(threshold in thresholds) { 
+   xes<-c(xes,x)
+}
+points(xes,thresholds)
+}
+if (sp==5) { 
+xes<-c()
+for(threshold in thresholds) { 
+   xes<-c(xes,x)
+}
+points(xes,thresholds,col="blue")
+}
+
+#      detect<-sum(cp.quadratic(thresholds)[-1]) 
+#     same result as 
+      detect = pUnionAnyOneOf(thresholds)
+      p<-runif(1)
+      if (p<detect) { hits<-hits+1 } 
    } 
-   coverage = (halfSweepWidth*4)/(1*width)
-   pod<- hits/(hits+misses)
+   coverage = (sweepWidth*sp)/(width)
+   pod<-(hits/tries)
    coverages<-c(coverages,coverage)
    pods<-c(pods,pod)
 } 
-plot(coverages,pods,type="l",col="green",xlim=c(0,2.0),ylim=c(0,1))
-lines(coverage_exp,pod_exp,col="blue")
-lines(coverage_pb,pod_pb,col="black")
+dev.off()
+
+coveragesFirst <- coverages
+podsFirst <- pods
+
+
+#coverages<-c()
+#pods<-c()
+#widths<-c(1.5,2,2.5,3,3.5,4,5,6,7,8,9,10,11,12,14,16,20,24,28,36,46,66)
+#f<-function(x) {exp(-abs(x^2))}
+#area<-integrate(f,-100,100)
+#halfSweepWidth<-area$value/2
+#for (width in widths) {
+#   hits=0
+#   misses=0
+#   spacing = width/2
+#   f1<-function(x) {exp(-abs((x-spacing)^2))}
+#   f2<-function(x) {exp(-abs(((x-(spacing*2)^2))))}
+#   f3<-function(x) {exp(-abs(((x-(spacing*3)^2))))}
+#   f4<-function(x) {exp(-abs(((x-(spacing*-1)^2))))}
+#   for (i in 1:10000) {
+#      x <- runif(1,0,width)
+#      detect <- f(x)
+#      detect1 <- f1(x)
+#      detect2 <- f2(x)
+#      detect3 <- f3(x)
+#      detect4 <- f4(x)
+#      p<-runif(1,0,1)
+#      if (p<detect) { hits<-hits+1 } else {
+#         if (p<detect1) { hits<-hits+1 } else {
+#           if (p<detect2) { hits<-hits+1 } else {
+#           if (p<detect3) { hits<-hits+1 } else {
+#           if (p<detect4) { hits<-hits+1 } else {
+#             misses<-misses+1
+#           }
+#           }
+#           }
+#         }
+#      }
+#   }
+#   coverage = (halfSweepWidth*4)/(1*width)
+#   pod<- hits/(hits+misses)
+#   coverages<-c(coverages,coverage)
+#   pods<-c(pods,pod)
+#}
+#lines(coverages,pods,type="l",col="red")
+
+graphpcs2<-"coveragecurves2.png"
+png(file.path(outputDirectory,graphpcs2),width=1200,height=1200)
+x<-c(0,10)
+y<-c(0,1)
+plot(x,y,type="p",xlim=c(0,10),ylim=c(0,1),main="Normal exp(-|x^2|)")
 
 coverages<-c()
 pods<-c()
-widths<-c(1.5,2,2.5,3,3.5,4,5,6,7,8,9,10,11,12,14,16,20,24,28,36,46,66)
+sweepsPerWidth<-c(1,2,3,4,5,6,7,8,9,10)
+width<-10
 f<-function(x) {exp(-abs(x^2))}
 area<-integrate(f,-100,100)
-halfSweepWidth<-area$value/2
-for (width in widths) {
+sweepWidth<-area$value
+f<-function(x,s) { exp(-abs((x-s)^2)) }
+for (sp in sweepsPerWidth) {
+   spacing = width / sp
    hits=0
-   misses=0
-   spacing = width/2
-   f1<-function(x) {exp(-abs((x-spacing)^2))}
-   f2<-function(x) {exp(-abs(((x-(spacing*2)^2))))}
-   f3<-function(x) {exp(-abs(((x-(spacing*3)^2))))}
-   f4<-function(x) {exp(-abs(((x-(spacing*-1)^2))))}
-   for (i in 1:10000) {
-      x <- runif(1,0,width)
-      detect <- f(x)
-      detect1 <- f1(x)
-      detect2 <- f2(x)
-      detect3 <- f3(x)
-      detect4 <- f4(x)
-      p<-runif(1,0,1)
-      if (p<detect) { hits<-hits+1 } else {
-         if (p<detect1) { hits<-hits+1 } else {
-           if (p<detect2) { hits<-hits+1 } else {
-           if (p<detect3) { hits<-hits+1 } else {
-           if (p<detect4) { hits<-hits+1 } else {
-             misses<-misses+1
-           }
-           }
-           }
-         }
-      }
+   tries=10000
+   for (i in 1:tries) {
+      x <- runif(1,min=0,max=width)
+      thresholds <- lp(f,x,spacing)
+
+if (sp==1) {
+xes<-c()
+for(threshold in thresholds) {
+   xes<-c(xes,x)
+}
+points(xes,thresholds)
+}
+if (sp==5) {
+xes<-c()
+for(threshold in thresholds) {
+   xes<-c(xes,x)
+}
+points(xes,thresholds,col="blue")
+}
+
+#      detect<-sum(cp.quadratic(thresholds)[-1]) 
+      detect = pUnionAnyOneOf(thresholds)
+      p<-runif(1)
+      if (p<detect) { hits<-hits+1 }
    }
-   coverage = (halfSweepWidth*4)/(1*width)
-   pod<- hits/(hits+misses)
+   coverage = (sweepWidth*sp)/(width)
+   pod<-(hits/tries)
    coverages<-c(coverages,coverage)
    pods<-c(pods,pod)
 }
-lines(coverages,pods,type="l",col="red")
+dev.off()
 
+coveragesSecond <- coverages
+podsSecond <- pods
+
+
+graphpcs3<-"coveragecurves3.png"
+png(file.path(outputDirectory,graphpcs3),width=1200,height=1200)
+x<-c(0,10)
+y<-c(0,1)
+plot(x,y,type="p",xlim=c(0,10),ylim=c(0,1),main="Normal exp(-|x^10|)*0.9")
 
 coverages<-c()
 pods<-c()
-widths<-c(1.5,2,2.5,3,3.5,4,5,6,7,8,9,10,11,12,14,16,20,24,28,36,46,66)
-f<-function(x) { ifelse( x>=1 | x<=-1 ,0,1) }
+sweepsPerWidth<-c(1,2,3,4,5,6,7,8,9,10)
+width<-10
+f<-function(x) { exp(-abs(x^10))*0.9 }
 area<-integrate(f,-100,100)
-halfSweepWidth<-area$value/2
-for (width in widths) {
+sweepWidth<-area$value
+f<-function(x,s) { exp(-abs((x-s)^10)) * 0.9 }
+for (sp in sweepsPerWidth) {
+   spacing = width / sp
    hits=0
-   misses=0
-   spacing = width/2
-   f1<-function(x) { ifelse( x>=1+spacing | x<=-1+spacing ,0,1) }
-   f2<-function(x) { ifelse( x>=1+spacing*2 | x<=-1+2*spacing ,0,1) }
-   f3<-function(x) { ifelse( x>=1+spacing*3 | x<=-1+3*spacing ,0,1) }
-   f4<-function(x) { ifelse( x>=1-spacing | x<=-1-spacing ,0,1) }
-   for (i in 1:10000) {
-      x <- runif(1,0,width)
-      detect <- f(x)
-      detect1 <- f1(x)
-      detect2 <- f2(x)
-      detect3 <- f3(x)
-      detect4 <- f4(x)
-      p<-runif(1,0,1)
-      if (p<detect) { hits<-hits+1 } else {
-         if (p<detect1) { hits<-hits+1 } else {
-           if (p<detect2) { hits<-hits+1 } else {
-           if (p<detect3) { hits<-hits+1 } else {
-           if (p<detect4) { hits<-hits+1 } else {
-             misses<-misses+1
-           }
-           }
-           }
-         }
-      }
+   tries=10000
+   for (i in 1:tries) {
+      x <- runif(1,min=0,max=width)
+      thresholds <- lp(f,x,spacing)
+
+if (sp==1) {
+xes<-c()
+for(threshold in thresholds) {
+   xes<-c(xes,x)
+}
+points(xes,thresholds)
+}
+if (sp==5) {
+xes<-c()
+for(threshold in thresholds) {
+   xes<-c(xes,x)
+}
+points(xes,thresholds,col="blue")
+}
+
+#      detect<-sum(cp.quadratic(thresholds)[-1]) 
+      detect = pUnionAnyOneOf(thresholds)
+      p<-runif(1)
+      if (p<detect) { hits<-hits+1 }
    }
-   coverage = (halfSweepWidth*4)/(1*width)
-   pod<- hits/(hits+misses)
+   coverage = (sweepWidth*sp)/(width)
+   pod<-(hits/tries)
    coverages<-c(coverages,coverage)
    pods<-c(pods,pod)
 }
-lines(coverages,pods,type="l",col="blue")
+dev.off()
+
+coveragesThird <- coverages
+podsThird <- pods
+
+
+
+graphpcs<-"coveragepodsim.png"
+png(file.path(outputDirectory,graphpcs),width=1200,height=1200)
+plot(coveragesFirst,podsFirst,type="l",col="green",xlim=c(0,2.0),ylim=c(0,1))
+lines(coverage_exp,pod_exp,col="blue")
+lines(coverage_pb,pod_pb,col="black")
+lines(coveragesSecond,podsSecond,type="l",col="red",xlim=c(0,2.0),ylim=c(0,1))
+lines(coveragesThird,podsThird,type="l",col="red",xlim=c(0,2.0),ylim=c(0,1))
+
+#
+#coverages<-c()
+#pods<-c()
+#widths<-c(1.5,2,2.5,3,3.5,4,5,6,7,8,9,10,11,12,14,16,20,24,28,36,46,66)
+#f<-function(x) { ifelse( x>=1 | x<=-1 ,0,1) }
+#area<-integrate(f,-100,100)
+#halfSweepWidth<-area$value/2
+#for (width in widths) {
+#   hits=0
+#   misses=0
+#   spacing = width/2
+#   f1<-function(x) { ifelse( x>=1+spacing | x<=-1+spacing ,0,1) }
+#   f2<-function(x) { ifelse( x>=1+spacing*2 | x<=-1+2*spacing ,0,1) }
+#   f3<-function(x) { ifelse( x>=1+spacing*3 | x<=-1+3*spacing ,0,1) }
+#   f4<-function(x) { ifelse( x>=1-spacing | x<=-1-spacing ,0,1) }
+#   for (i in 1:10000) {
+#      x <- runif(1,0,width)
+#      detect <- f(x)
+#      detect1 <- f1(x)
+#      detect2 <- f2(x)
+#      detect3 <- f3(x)
+#      detect4 <- f4(x)
+#      p<-runif(1,0,1)
+#      if (p<detect) { hits<-hits+1 } else {
+#         if (p<detect1) { hits<-hits+1 } else {
+#           if (p<detect2) { hits<-hits+1 } else {
+#           if (p<detect3) { hits<-hits+1 } else {
+#           if (p<detect4) { hits<-hits+1 } else {
+#             misses<-misses+1
+#           }
+#           }
+#           }
+#         }
+#      }
+#   }
+#   coverage = (halfSweepWidth*4)/(1*width)
+#   pod<- hits/(hits+misses)
+#   coverages<-c(coverages,coverage)
+#   pods<-c(pods,pod)
+#}
+#lines(coverages,pods,type="l",col="blue")
+
+coverages<-c()
+pods<-c()
+sweepsPerWidth<-c(1,2,3,4,5,6,7,8,9,10)
+width<-10
+f<-function(x) { ifelse( x>=1 | x<=-1 ,0,1) }
+area<-integrate(f,-100,100)
+sweepWidth<-area$value
+f<-function(x,s) { ifelse( x>=1+s | x<=-1+s ,0,1) }
+for (sp in sweepsPerWidth) { 
+   spacing = width / sp
+   hits=0
+   tries=10000
+   for (i in 1:tries) { 
+      x <- runif(1,min=0,max=width)
+      thresholds <- lp(f,x,spacing)
+#      detect<-sum(cp.quadratic(thresholds)[-1]) 
+      detect = pUnionAnyOneOf(thresholds)
+      p<-runif(1)
+      if (p<detect) { hits<-hits+1 } 
+   } 
+   coverage = (sweepWidth*sp)/(width)
+   pod<-(hits/tries)
+   coverages<-c(coverages,coverage)
+   pods<-c(pods,pod)
+} 
+lines(coverages,pods,type="l",col="green",xlim=c(0,2.0),ylim=c(0,1))
 
 dev.off();
 HTMLInsertGraph(graphpcs,file=output,Caption="Coverage POD relationships by simulation for various detection fuctions")
